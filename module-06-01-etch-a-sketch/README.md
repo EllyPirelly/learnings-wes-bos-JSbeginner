49c1bd3d2c597fe7e01eaad9649d1de54c7fddd1
ehanschen <ehanschen@3pc.de>
2022-06-10 14:53:49 +0200

    clean-up

--- module-06-01-etch-a-sketch/README.md
+++ module-06-01-etch-a-sketch/README.md
@@ -7,36 +7,27 @@
 - Etch-a-Sketch
   - JavaScript, HTML, CSS
   - use arrow keys up, right, down, left, to move around
-  - line in rainbow colors
-  - click "Click to clear" to start over
+  - draw a line in rainbow colors - everytime an arrow key moves the cursor, increment the hue value by 10
+  - click "Click to clear" to start over (have a wobble effect applied to the canvas)
   - when reloading the page (`cmd + r`), starting spot is randomized
-
+  - only desktop, not mobile
 ## Topics to cover
 
 - DOM elements
 - keyboard events
-- canvas
+- `<canvas> ... </canvas>` (2d,  not 3d)
 - switch statement
+- hue colors
+- randomisation of starting spot
 
 **canvas**
 
-- grab canvas
-- grab context (which is either 2d or 3d)
 - the exercise is build with 2d context, see examples with [3d context here: ](https://threejs.org/), [for example ](https://threejs.org/examples/#webgl_effects_parallaxbarrier)
 - canvas is a set of APIs, set of methods, that are used for drawing
 - rectangles, circles, drawing, lines, borders, different fill colors, you name it - all those are available in canvas
 - programmatically draw something to the browser
 - `canvas` is the element
 - `context` is where the drawing happens
-
-```
-ctx.lineJoin = 'round';
-ctx.lineCap = 'round';
-ctx.lineWidth = 10;
-```
-
-- `'round'` to ensure a smooth drawing (default is a squared off edge)
-- `lineWidth` by default is 1px
 
 **keyboard events**
 
@@ -45,31 +36,10 @@ ctx.lineWidth = 10;
 
 **switch statement**
 
-- take in a variable, like the key, and depending on the four cases: go up, go down, go left or go right
+- take in a variable, like the key, and depending on the four cases it might go up, go down, go left or go right
 - a switch statement always needs a default case
 - `break` will stop the switch from running and skip over the `false` statements
 
 **colors**
-
-- everytime an arrow key moves the cursor, increment the H (hue) value by one
-
-```
-let hue = 0;
-ctx.strokeStyle = `hsl(100, 100%, 50%)`;
-
-function draw({ key }) {
-    // increment hue by 10
-    hue = hue + 10;
-    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-    ...
-}
-```
-
-- explicitly update the `strokeStyle` by 10
-
-```
-// randomise colors
-ctx.strokeStyle = `hsl(${Math.random() * 360}, 100%, 50%)`;
-```
 
 - [Mother-effing hsl()](https://mothereffinghsl.com/)
--- module-06-01-etch-a-sketch/etch-a-sketch.js
+++ module-06-01-etch-a-sketch/etch-a-sketch.js
@@ -9,7 +9,7 @@ const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
 /* const { canvasWidth, canvasHeight } = canvas; // destructuring */
 
-// create randomised starting spot
+// create randomised starting spot x and y
 let x = Math.floor(Math.random() * canvasWidth);
 let y = Math.floor(Math.random() * canvasHeight);
 
@@ -18,65 +18,78 @@ ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = FIXED_AMOUNT;
 
-// set line style rainbow color, starting point, draw starting point
+// set line style hue
 let hue = 0;
+// set line style base color before rainbow
 ctx.strokeStyle = `hsl(100, 100%, 50%)`;
-// start drawing/"set pencil on surface"
+// starting point, start drawing/"set pencil on surface"
 ctx.beginPath();
 // starting x and y (left and top)
 ctx.moveTo(x, y);
+// spot, where you draw the line to
 ctx.lineTo(x, y);
+// draw
 ctx.stroke();
 
 // handler function for arrow keys
 function handleKey(eve) {
-    if (eve.key.includes('Arrow')) {
-        eve.preventDefault();
-        draw({ key: eve.key });
-    }
+  if (eve.key.includes('Arrow')) {
+    eve.preventDefault();
+    draw({ key: eve.key });
+  }
 }
 
 // draw function
 function draw({ key }) {
-    // increment hue by 10
-    hue = hue + 10;
-    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-    /* ctx.strokeStyle = `hsl(${Math.random() * 360}, 100%, 50%)`; // randomise colors */
-    // start path
-    ctx.beginPath();
-    // again set original starting position
-    ctx.moveTo(x, y);
-    // move x and y depending on user action
-    switch (key) {
-        case 'ArrowUp':
-            y = y - FIXED_AMOUNT;
-            break;
-        case 'ArrowRight':
-            x = x + FIXED_AMOUNT;
-            break;
-        case 'ArrowDown':
-            y = y + FIXED_AMOUNT;
-            break;
-        case 'ArrowLeft':
-            x = x - FIXED_AMOUNT;
-            break;
-        default:
-            break;
-    }
-    ctx.lineTo(x, y);
-    ctx.stroke();
+  // increment hue by 10 to create rainbow line
+  hue = hue + 10;
+  // explicitly updating the stroke style
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  // randomise colors if wanted
+  /* ctx.strokeStyle = `hsl(${Math.random() * 360}, 100%, 50%)`; */
+  // starting point, start drawing/"set pencil on surface"
+  ctx.beginPath();
+  // again set original starting position
+  ctx.moveTo(x, y);
+
+  // move x and y depending on user action
+  switch (key) {
+    case 'ArrowUp':
+      y = y - FIXED_AMOUNT;
+      break;
+    case 'ArrowRight':
+      x = x + FIXED_AMOUNT;
+      break;
+    case 'ArrowDown':
+      y = y + FIXED_AMOUNT;
+      break;
+    case 'ArrowLeft':
+      x = x - FIXED_AMOUNT;
+      break;
+    default:
+      break;
+  }
+
+  // spot, where you draw the line to
+  ctx.lineTo(x, y);
+  // draw
+  ctx.stroke();
 }
 
-// clear / wobble function
+// clear function that also wobbles the canvas when clicked
 function clearCanvas() {
-    canvas.classList.add('wobble');
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    canvas.addEventListener('animationend', function () {
-        canvas.classList.remove('wobble');
-    }, { once: true });
+
+  canvas.classList.add('wobble');
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+  // listen to the animation to be finished (animation end) to remove the wobble class - to be able to run that animation again
+  canvas.addEventListener('animationend', function () {
+    canvas.classList.remove('wobble');
+  }, { once: true });
 }
 
 // listen for arrow keys
 window.addEventListener('keydown', handleKey);
+
 // hook up the shake button to clear canvas
 shakeButton.addEventListener('click', clearCanvas);
